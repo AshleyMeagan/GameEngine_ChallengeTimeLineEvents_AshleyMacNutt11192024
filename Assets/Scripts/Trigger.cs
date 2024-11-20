d341765a92d60e261b463932325953c52be0f1e6
AshleyMeagan <macnutta@live.com>
2024-11-20 12:18:02 -0400

    omg.. it works..

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class Trigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   public PlayableDirector timeline;
+   private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+      timeline.Play();  
     }
 }
